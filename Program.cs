d6627fbd88f42aeb4dd61be019dea9b8cb4c47f5
Rufeldi-MIANTSADI <rufinamiantsadi@gmail.com>
2026-04-24 00:27:43 +0100

    premier commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Table_de_multiplication;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+        int i;
+        
+        int resultat;
+
+        Console.WriteLine("saisir un nombre de votre choix");
+        N=Convert.ToInt32(Console.ReadLine());
+        for(i =1;i<=10;i++)
+        {
+            resultat=N*i;
+            Console.WriteLine(N+"x"+i+"="+resultat);
+        }
+    }
+}
